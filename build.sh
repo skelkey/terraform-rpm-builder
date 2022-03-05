3e699db799076aaf6cc9f820fa892e7ce276d9d4
Edouard Camoin <edouard.camoin@gmail.com>
2022-03-05 22:20:20 +0000

    Finalizing build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+TIMEOUT=5
+RPM_PATH=/tmp/rpmbuild/RPMS/x86_64
+
 fedora28_rsa=$(mktemp /tmp/XXXXXX_fedora28_keypair.rsa)
 fedora33_rsa=$(mktemp /tmp/XXXXXX_fedora33_keypair.rsa)
 
@@ -11,9 +14,43 @@ fedora33_ip=$(terraform output --raw fedora33_rpm_builder)
 terraform output --raw fedora33_keypair > $fedora33_rsa
 terraform output --raw fedora28_keypair > $fedora28_rsa
 
-echo "ssh -i $fedora28_rsa fedora@$fedora28_ip"
-echo "ssh -i $fedora33_rsa fedora@$fedora33_ip"
-#scp fedora28
-#scp fedora33
+echo "Fedora 28 : ssh -i $fedora28_rsa fedora@$fedora28_ip"
+echo "Fedora 33 : ssh -i $fedora33_rsa fedora@$fedora33_ip"
 
-#terraform destroy --auto-approve
+for i in $(seq 1 $TIMEOUT)
+do
+    echo -e "Try #$i (waiting $i minutes()) ..."
+    sleep ${i}m
+    if ssh -o "StrictHostKeyChecking no" -i $fedora28_rsa fedora@$fedora28_ip test -e /tmp/done
+    then
+        scp -o "StrictHostKeyChecking no" -i $fedora28_rsa fedora@$fedora28_ip:$RPM_PATH/*.rpm /tmp
+	echo "OK"
+        break
+    fi
+    echo "Failed"
+done
+if test $i -eq $TIMEOUT
+then
+    echo "Failed to download RPMS from fedora 28"
+fi
+
+for i in $(seq 1 $TIMEOUT)
+do
+    echo -e "Try #$i (waiting $i minutes()) ..."
+    sleep ${i}m
+    if ssh -o "StrictHostKeyChecking no" -i $fedora33_rsa fedora@$fedora33_ip test -e /tmp/done
+    then
+        scp -o "StrictHostKeyChecking no" -i $fedora33_rsa fedora@$fedora33_ip:$RPM_PATH/*.rpm /tmp
+	echo "OK"
+        break
+    fi
+    echo "Failed"
+done  
+if test $i -eq $TIMEOUT
+then
+    echo "Failed to download RPMS from fedora 33"
+fi
+
+terraform destroy --auto-approve
+rm -f $fedora33_rsa
+rm -f $fedora28_rsa
--- build.tpl
+++ build.tpl
@@ -2,3 +2,4 @@ runcmd:
   - git clone ${git_address} /tmp/rpmbuild
   - spectool -g -C /tmp/rpmbuild/SOURCES /tmp/rpmbuild/SPECS/${package_name}.spec
   - rpmbuild -ba --define "_topdir /tmp/rpmbuild" /tmp/rpmbuild/SPECS/${package_name}.spec
+  - touch /tmp/done
